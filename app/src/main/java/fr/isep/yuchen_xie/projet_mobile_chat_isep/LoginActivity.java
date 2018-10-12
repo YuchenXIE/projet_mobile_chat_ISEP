4be5d4ca64daf63b0d31c13f0112f58efc4b9748
YuchenXIE <yuchen_x@yahoo.fr>
2018-10-12 10:55:14 +0200

    Début

---
+++ app/src/main/java/fr/isep/yuchen_xie/projet_mobile_chat_isep/LoginActivity.java
@@ -0,0 +1,13 @@
+package fr.isep.yuchen_xie.projet_mobile_chat_isep;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
