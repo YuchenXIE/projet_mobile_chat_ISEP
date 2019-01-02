f81fb1250502ee1cf15842c45040c529fdda4c2e
ahmedmdallel <ahmed.mdallel@esprit.tn>
2019-01-02 17:10:37 +0100

    mdalel commit 1

--- app/src/main/java/fr/isep/yuchen_xie/projet_mobile_chat_isep/LoginActivity.java
+++ app/src/main/java/fr/isep/yuchen_xie/projet_mobile_chat_isep/LoginActivity.java
@@ -1,13 +1,25 @@
 package fr.isep.yuchen_xie.projet_mobile_chat_isep;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
-
+     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
