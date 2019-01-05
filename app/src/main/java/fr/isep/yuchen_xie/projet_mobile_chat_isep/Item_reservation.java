a5e65ea7680d646c6880f2a7f79f1c1b95aae26b
YuchenXIE <yuchen_x@yahoo.fr>
2019-01-05 18:56:53 +0100

    V1

---
+++ app/src/main/java/fr/isep/yuchen_xie/projet_mobile_chat_isep/Item_reservation.java
@@ -0,0 +1,13 @@
+package fr.isep.yuchen_xie.projet_mobile_chat_isep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Item_reservation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_reservation);
+    }
+}
