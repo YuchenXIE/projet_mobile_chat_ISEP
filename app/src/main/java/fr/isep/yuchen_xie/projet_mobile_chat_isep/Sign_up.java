9f4bec0a8025d635911ac6feb9373d6c009b2f68
ABLALTI <mohamed.blalti@isep.fr>
2019-01-07 16:43:30 +0100

    sign up  commit

--- app/src/main/java/fr/isep/yuchen_xie/projet_mobile_chat_isep/Sign_up.java
+++ app/src/main/java/fr/isep/yuchen_xie/projet_mobile_chat_isep/Sign_up.java
@@ -2,6 +2,8 @@ package fr.isep.yuchen_xie.projet_mobile_chat_isep;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Sign_up extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class Sign_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        final EditText FirstName = (EditText)findViewById(R.id.FirstName);
+        final EditText LasstName = (EditText)findViewById(R.id.LastName);
+        final EditText Email = (EditText)findViewById(R.id.Email);
+        final EditText Password = (EditText)findViewById(R.id.Password);
+        final EditText Phonenumber= (EditText)findViewById(R.id.PhoneNumber);
+        final Button BtnSU = (Button) findViewById(R.id.BtnSU);
     }
 }
